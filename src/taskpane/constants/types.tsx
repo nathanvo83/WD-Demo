8069c5e4f2c002045af87655039e3258aa27bf85
nathanvo83 <nathanvo83@gmail.com>
2020-05-06 17:18:46 +1200

    add types show/hide settingPane, Exclusions, Theme

--- src/taskpane/constants/types.tsx
+++ src/taskpane/constants/types.tsx
@@ -1,13 +1,20 @@
 export const types = {
+  // MO
   SET_CHUNK_DETAILS: "select chunk details",
   SET_CHUNK_LIST: "select chunk list",
   SET_SCORE: "set score",
+  SET_EXCLUSIONS: "SET_EXCLUSIONS",
+  SET_THEME: "SET_THEME",
+
+  // TRUE / FALSE
   SHOW_INFO_PANE: "show info pane",
   HIDE_INFO_PANE: "hide info pane",
   SHOW_FIRST_RUN: "show first run",
   HIDE_FIRST_RUN: "hide first run",
   SHOW_INTRO: "SHOW INTRO",
-  HIDE_INTRO: "HIDE INTRO"
+  HIDE_INTRO: "HIDE INTRO",
+  SHOW_SETTINGS_PANE: "SHOW_SETTINGS_PANE",
+  HIDE_SETTINGS_PANE: "HIDE_SETTINGS_PANE"
   // OPEN_CHUNK : 'open chunk',
   // CLOSE_CHUNK : 'close chunk'
 };
